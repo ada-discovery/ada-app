11d5ec9a5187e727c22ba10c624680f8a95f1997
Peter Banda <peter.banda@uni.lu>
2015-11-26 14:36:08 +0100

    Dictionary class

--- app/models/Dataset.scala
+++ app/models/Dataset.scala
@@ -1,8 +1,33 @@
 package models
 
-/**
-  * Created by peter on 26/11/15.
-  */
-class Dataset {
+import reactivemongo.bson.BSONObjectID
 
+case class Dictionary(
+  _id : Option[BSONObjectId],
+  dataSetName : String,
+  fields : Traversable[Field]
+  //  parents : List[Dictionary],
+)
+
+object FieldType extends Enumeration {
+  val Null, Boolean, Double, Integer, String, Date = Value
 }
+
+case class Field(
+  _id : Option[BSONObjectId],
+  name : String,
+  fieldType : FieldType,
+  aliases : List[String],
+  labels : List[String],
+  category : Category
+)
+
+
+
+
+//case class TypeStats {
+//  enum : Boolean,
+//  min :
+//}
+
+// def union(refSet : Dataset, anotherset : Dataset)
