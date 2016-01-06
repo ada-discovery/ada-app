5f77b0dc67db1a191348bed1be42ea5b1db04b5d
jan.martens <jan.martens@uni.lu>
2016-01-06 11:58:57 +0100

    documentation

--- app/persistence/AsyncRepo.scala
+++ app/persistence/AsyncRepo.scala
@@ -13,6 +13,17 @@ import scala.concurrent.Future
 trait AsyncReadonlyRepo[E, ID] {
   def get(id: ID): Future[Option[E]]
 
+  /**
+    * Gets all elements matching criteria object.
+    * Returned values are chunked according to the arguments for pagination.
+    *
+    * @param criteria Filtering criteria object. Use a String to filter according to value of reference column. Use None for no filtering.
+    * @param orderBy Column used as reference for sorting. Leave at None to use default.
+    * @param projection Defines which columns are supposed to be returned. Leave at None to use default.
+    * @param limit Page limit. Use to define chunk sizes. Leave at None to use default.
+    * @param page Page to be returned. Specifies which chunk is returned.
+    * @return Traversable object for iteration.
+    */
   def find(
     criteria: Option[JsObject] = None,
     orderBy: Option[JsObject] = None,
@@ -21,6 +32,12 @@ trait AsyncReadonlyRepo[E, ID] {
     page: Option[Int] = None
   ): Future[Traversable[E]]
 
+  /**
+    * Return the number of elements matching criteria.
+    *
+    * @param criteria Filtering criteria object. Use a String to filter according to value of reference column. Use None for no filtering.
+    * @return Number of maching elements.
+    */
   def count(criteria: Option[JsObject]) : Future[Int]
 
 //  def getDictionary : models.Dictionary
