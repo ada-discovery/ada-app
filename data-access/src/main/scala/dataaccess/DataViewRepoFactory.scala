761331006e3e27d05e94fe7dd00362b79dafbe11
Peter Banda <tremor11@gmail.com>
2016-11-28 10:32:27 +0100

    Closes #145. Adding a user field (created by) for data views.

--- data-access/src/main/scala/dataaccess/DataViewRepoFactory.scala
+++ data-access/src/main/scala/dataaccess/DataViewRepoFactory.scala
@@ -1,10 +1,37 @@
 package dataaccess
 
 import com.google.inject.ImplementedBy
-import dataaccess.RepoTypes.DataViewRepo
+import dataaccess.RepoTypes._
+import dataaccess.User.UserIdentity
 import dataaccess.ignite.DataViewCacheCrudRepoFactory
+import models.DataView
+import dataaccess.Criterion.CriterionInfix
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 @ImplementedBy(classOf[DataViewCacheCrudRepoFactory])
 trait DataViewRepoFactory {
   def apply(dataSetId: String): DataViewRepo
+}
+
+object DataViewRepo {
+
+  def setCreatedBy(
+    userRepo: UserRepo,
+    dataViews: Traversable[DataView]
+  ): Future[Unit] = {
+    val userIds = dataViews.map(_.createdById).flatten.map(Some(_)).toSeq
+
+    if (userIds.nonEmpty) {
+      userRepo.find(Seq(UserIdentity.name #=> userIds)).map { users =>
+        val userIdMap = users.map(c => (c._id.get, c)).toMap
+        dataViews.foreach(dataView =>
+          if (dataView.createdById.isDefined) {
+            dataView.createdBy = userIdMap.get(dataView.createdById.get)
+          }
+        )
+      }
+    } else
+      Future(())
+  }
 }
--- data-access/src/main/scala/models/DataView.scala
+++ data-access/src/main/scala/models/DataView.scala
@@ -1,13 +1,12 @@
 package models
 
-import dataaccess.{BSONObjectIdentity, EnumFormat}
-import play.api.libs.json.Json
+import dataaccess.{User, BSONObjectIdentity, EnumFormat, OptionFormat}
+import play.api.libs.functional.syntax._
 import reactivemongo.bson.BSONObjectID
 import models.DataSetFormattersAndIds.statsCalcSpecFormat
 import java.util.Date
 import play.api.libs.json._
 import reactivemongo.play.json.BSONFormats._
-import dataaccess.OptionFormat
 
 case class DataView(
   _id: Option[BSONObjectID],
@@ -18,13 +17,27 @@ case class DataView(
   elementGridWidth: Int = 3,
   default: Boolean = false,
   createdById: Option[BSONObjectID] = None,
-  timeCreated: Date = new Date()
+  timeCreated: Date = new Date(),
+  var createdBy: Option[User] = None
 )
 
 object DataView {
   // TODO: weird that we had to use a custom option format here..
   implicit val optionalBSONObjectIdFormat = new OptionFormat[BSONObjectID]
-  implicit val dataViewFormat = Json.format[DataView]
+  implicit val dataViewFormat : Format[DataView] = (
+    (__ \ "_id").formatNullable[BSONObjectID] and
+    (__ \ "name").format[String] and
+    (__ \ "filterIds").format[Seq[Option[BSONObjectID]]] and
+    (__ \ "tableColumnNames").format[Seq[String]] and
+    (__ \ "statsCalcSpecs").format[Seq[StatsCalcSpec]] and
+    (__ \ "elementGridWidth").format[Int] and
+    (__ \ "default").format[Boolean] and
+    (__ \ "createdById").formatNullable[BSONObjectID] and
+    (__ \ "timeCreated").format[Date]
+  )(
+    DataView(_, _, _, _, _, _, _, _, _),
+    (item: DataView) =>  (item._id, item.name, item.filterIds, item.tableColumnNames, item.statsCalcSpecs, item.elementGridWidth, item.default, item.createdById, item.timeCreated)
+  )
 
   implicit object DataViewIdentity extends BSONObjectIdentity[DataView] {
     def of(entity: DataView): Option[BSONObjectID] = entity._id
