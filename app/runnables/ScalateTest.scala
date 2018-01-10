727fa98e0a7e5bd23861aa1aaf8a8b07fd3f822a
Peter Banda <tremor11@gmail.com>
2018-01-10 11:18:36 +0100

    Fixing message repo stream and web event stream

--- app/runnables/ScalateTest.scala
+++ app/runnables/ScalateTest.scala
@@ -1,9 +1,9 @@
 package runnables
 
-import org.fusesource.scalate._
+// import org.fusesource.scalate._
 
 object ScalateTest extends App {
-  val engine = new TemplateEngine
-  val output = engine.layout("bar.scaml", Map("name" -> ("Hiram", "Chirino"), "city" -> "Tampa"))
-  println(output)
+//  val engine = new TemplateEngine
+//  val output = engine.layout("bar.scaml", Map("name" -> ("Hiram", "Chirino"), "city" -> "Tampa"))
+//  println(output)
 }
