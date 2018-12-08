57ff3d9b75dff71e43cd976c774c0e0cc9d9b4d3
Peter Banda <tremor11@gmail.com>
2018-12-08 18:16:38 +0100

    Custom dir (lookup) asset manager added.

--- app/controllers/CustomDirAssets.scala
+++ app/controllers/CustomDirAssets.scala
@@ -3,9 +3,9 @@ package controllers
 import javax.inject.{Inject, Singleton}
 
 import controllers.Assets.Asset
-import play.api.{Configuration, Logger}
-import play.api.mvc.{Action, Result}
-import play.api.http.{HttpErrorHandler, Status}
+import play.api.Configuration
+import play.api.mvc.Result
+import play.api.http.Status
 import play.api.mvc.Results.{BadRequest, NotFound}
 import play.api.mvc.AnyContent
 import play.api.mvc.Action
@@ -17,22 +17,20 @@ import scala.concurrent.Future
 class CustomDirAssets @Inject() (assets: Assets, configuration: Configuration) {
 
   private val externalAssetPaths = configuration.getStringSeq("assets.external_paths").getOrElse(Nil).toList
-  private val logger = Logger
 
   def versioned(
     primaryPath: String,
     file: Asset
-  ) = findAssetAux(primaryPath :: externalAssetPaths, file.name, Assets.versioned(_, file))
+  ) = findAssetAux(primaryPath :: externalAssetPaths, Assets.versioned(_, file))
 
   def at(
     primaryPath: String,
     file: String,
     aggressiveCaching: Boolean = false
-  ) = findAssetAux(primaryPath :: externalAssetPaths, file, Assets.at(_, file, aggressiveCaching))
+  ) = findAssetAux(primaryPath :: externalAssetPaths, Assets.at(_, file, aggressiveCaching))
 
   private def findAssetAux(
     paths: Seq[String],
-    fileName: String,
     assetAction: String => Action[AnyContent]
   ) = Action.async { implicit request =>
     def isNotFound(result: Result) = result.header.status == Status.NOT_FOUND
@@ -44,12 +42,7 @@ class CustomDirAssets @Inject() (assets: Assets, configuration: Configuration) {
         Future(NotFound: Result)
       )(
         (resultFuture, path) => resultFuture.flatMap( result =>
-          if (isNotFound(result)) {
-            assetAction(path)(request).map { result =>
-              logger.info(s"Finding $fileName at the path $path with a status ${result.header.status}.")
-              result
-            }
-          } else Future(result)
+          if (isNotFound(result)) assetAction(path)(request) else Future(result)
         )
       )
   }
