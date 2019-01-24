f26ac57e1b11823186670e2d2ab78e73f30b0519
Peter Banda <tremor11@gmail.com>
2019-01-24 13:10:45 +0100

    Replace number for a data set script

--- app/runnables/core/ReplaceNumber.scala
+++ app/runnables/core/ReplaceNumber.scala
@@ -1,23 +1,25 @@
 package runnables.core
 
-import dataaccess.RepoTypes.{FieldRepo, JsonCrudRepo}
-import field.{FieldType, FieldTypeHelper}
-import models.DataSetFormattersAndIds.JsObjectIdentity
-import models.{AdaException, Field, FieldTypeId}
-import org.incal.core.util.seqFutures
-import play.api.libs.json.{JsNull, JsString, JsValue, Json}
-import reactivemongo.bson.BSONObjectID
-import reactivemongo.play.json.BSONFormats._
+import dataaccess.RepoTypes.JsonCrudRepo
+import models.AdaException
+import play.api.libs.json._
 import runnables.DsaInputFutureRunnable
-import util.FieldUtil.{InfixFieldOps, JsonFieldOps, NamedFieldType}
+import _root_.util.FieldUtil.InfixFieldOps
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Flow
+import dataaccess.JsonCrudRepoExtra._
+import dataaccess.StreamSpec
+import org.incal.core.dataaccess.NotEqualsNullCriterion
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
 import scala.reflect.runtime.universe.typeOf
 
 class ReplaceNumber extends DsaInputFutureRunnable[ReplaceNumberSpec] {
 
-  private val idName = JsObjectIdentity.name
+  private implicit val system = ActorSystem()
+  private implicit val materializer = ActorMaterializer()
+  private val flatFlow = Flow[Option[JsObject]].collect { case Some(x) => x }
 
   override def runAsFuture(spec: ReplaceNumberSpec) = {
     val dsa = createDsa(spec.dataSetId)
@@ -27,63 +29,37 @@ class ReplaceNumber extends DsaInputFutureRunnable[ReplaceNumberSpec] {
       fieldOption <- dsa.fieldRepo.get(spec.fieldName)
       field = fieldOption.getOrElse(throw new AdaException(s"Field ${spec.fieldName} not found."))
 
-      // replace for String or Enum
-      _ <- if (!field.isArray && (field.isNumeric || field.isEnum)) {
-
-      } else {
-        throw new AdaException(s"Number replacement is possible only for double, integer, date, an enum types but got ${field.fieldTypeSpec}.")
-      }
+      // replace for numbers or eum
+      _ <- if (!field.isArray && (field.isNumeric || field.isEnum))
+          replaceNumber(dsa.dataSetRepo, spec)
+        else
+          throw new AdaException(s"Number replacement is possible only for double, integer, date, an enum types but got ${field.fieldTypeSpec}.")
     } yield
       ()
   }
 
-  private def replaceForString(
+  private def replaceNumber(
     repo: JsonCrudRepo,
-    fieldType: NamedFieldType[String],
     spec: ReplaceNumberSpec
-  ) = {
+  ) =
     for {
-      // jsons
-      idJsons <- repo.find(projection = Seq(idName, spec.fieldName))
+      // input stream
+      inputStream <- repo.findAsStream(Seq(NotEqualsNullCriterion(spec.fieldName)))
 
-      // get the records as String and replace
-      idReplacedStringValues = idJsons.map { json =>
-        val id = (json \ JsObjectIdentity.name).as[BSONObjectID]
-        val replacedStringValue = json.toValue(fieldType).map(_.replaceAllLiterally(spec.from, spec.to))
-        (id, replacedStringValue)
-      }
+      // replaced stream
+      replacedStream = inputStream.map( json =>
+        (json \ spec.fieldName).get match {
+          case JsNumber(value) if value.equals(spec.from) => Some(json.+(spec.fieldName, JsNumber(spec.to)))
+          case _ =>  None
+        }
+      )
 
-      // replace the values and update the records
-      _ <- seqFutures(idReplacedStringValues.toSeq.grouped(spec.batchSize)) { group =>
-        for {
-          newJsons <- Future.sequence(
-            group.map { case (id, replacedStringValue) =>
-              repo.get(id).map { json =>
-                val jsValue: JsValue = replacedStringValue.map(JsString(_)).getOrElse(JsNull)
-                json.get ++ Json.obj(spec.fieldName -> jsValue)
-              }
-            }
-          )
-
-          _ <- repo.update(newJsons)
-        } yield
-          ()
-      }
+      // update the replaced jsons as stream
+      _ <- repo.updateAsStream(replacedStream.via(flatFlow), spec.updateStreamSpec)
     } yield
       ()
-  }
-
-  private def replaceForEnum(
-    repo: FieldRepo,
-    field: Field,
-    spec: ReplaceNumberSpec
-  ) = {
-    val newNumValue = field.numValues.map(_.map { case (key, value) => (key, value.replaceAllLiterally(spec.from, spec.to))})
-
-    repo.update(field.copy(numValues = newNumValue))
-  }
 
   override def inputType = typeOf[ReplaceNumberSpec]
 }
 
-case class ReplaceNumberSpec(dataSetId: String, fieldName: String, batchSize: Int, from: String, to: String)
+case class ReplaceNumberSpec(dataSetId: String, fieldName: String, from: Double, to: Double, updateStreamSpec: StreamSpec)
