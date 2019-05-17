199086f0aa11271d1bedd3903140860322b26912
Emanuele RAFFERO <emanuele.raffero@gmail.com>
2019-05-17 10:53:47 +0200

    add batchrequest runnables

--- app/runnables/other/GetRequestListRun.scala
+++ app/runnables/other/GetRequestListRun.scala
@@ -3,6 +3,7 @@ package runnables.other
 import javax.inject.Inject
 import org.incal.core.runnables.{FutureRunnable, RunnableHtmlOutput}
 import services.BatchRequestRepoTypes.BatchRequestRepo
+import views.html.requests.list
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -13,7 +14,15 @@ class GetRequestListRun @Inject()(requestsRepo: BatchRequestRepo)
     for {
       requestRead <- requestsRepo.find()
     } yield {
-      addParagraph(bold(requestRead.toString))
+
+
+//      val requests: Seq[BSONObjectID]=requestRead.map(request=>request._id.get).toSeq
+
+
+    // val page = Page(requests, 0, 0, requests.size,"")
+      val html = list(requestRead.toSeq).toString()
+      addDiv(html)
+     // addParagraph(bold(requestRead.toString))
     }
   }
 }
