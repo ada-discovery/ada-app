46a29eb598ebeb1ba3be143fa60a3261f8bb5c7c
Peter Banda <tremor11@gmail.com>
2019-05-17 15:09:09 +0200

    Small cleanup of batch request runnables.

--- test/services/ApprovalCommitteeRepoTest.scala
+++ test/services/ApprovalCommitteeRepoTest.scala
@@ -7,7 +7,7 @@ import org.ada.server.models.DataSetFormattersAndIds.serializableBSONObjectIDFor
 import org.ada.server.services.StatsService
 import org.scalatest.{AsyncFlatSpec, Matchers}
 import reactivemongo.bson.BSONObjectID
-import services.BatchRequestRepoTypes.ApprovalCommitteeRepo
+import services.BatchOrderRequestRepoTypes.ApprovalCommitteeRepo
 
 
 import play.api.test._
@@ -16,7 +16,7 @@ import play.api.test._
 class ApprovalCommitteeRepoTest extends AsyncFlatSpec with Matchers with ExtraMatchers {
 
   private val injector = TestApp(Seq("play.modules.reactivemongo.ReactiveMongoModule")).injector
-  private val statsService = injector.instanceOf[BatchRequestRepoTypes.ApprovalCommitteeRepo]
+  private val statsService = injector.instanceOf[BatchOrderRequestRepoTypes.ApprovalCommitteeRepo]
 
   "add new committee" should "success" in {
     assert(true===false)
