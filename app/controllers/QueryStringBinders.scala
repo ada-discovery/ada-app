187d3ddd1939cb29990a55addb75246499c06e26
Emanuele RAFFERO <emanuele.raffero@gmail.com>
2019-07-26 11:10:13 +0200

    fix conflicts with master

--- app/controllers/QueryStringBinders.scala
+++ app/controllers/QueryStringBinders.scala
@@ -1,9 +1,8 @@
 package controllers
 
-import models.{BatchRequestState, RequestAction, Role}
+import models.{Role}
 import org.ada.web.controllers.EnumStringBindable
 import models.{BatchRequestState, RequestAction}
-import org.incal.play.formatters.EnumStringBindable
 
 object QueryStringBinders extends scala.AnyRef {
   implicit val batchRequestStateStringBinder = new EnumStringBindable(BatchRequestState)
