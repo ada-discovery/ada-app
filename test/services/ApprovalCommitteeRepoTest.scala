5415826fba38ba8f3a43e0d32dab98e4f266d933
Peter Banda <tremor11@gmail.com>
2019-12-13 15:31:25 +0100

    Replacing displayColumns and widget specs in batchOrderRequestSetting with a viewId.

--- test/services/ApprovalCommitteeRepoTest.scala
+++ test/services/ApprovalCommitteeRepoTest.scala
@@ -6,7 +6,7 @@ import org.ada.server.models.DataSetFormattersAndIds.serializableBSONObjectIDFor
 import org.ada.server.services.{GuicePlayTestApp, StatsService}
 import org.scalatest.{FlatSpec, Matchers}
 import reactivemongo.bson.BSONObjectID
-import services.BatchOrderRequestRepoTypes.RequestSettingRepo
+import services.BatchOrderRequestRepoTypes.BatchOrderRequestSettingRepo
 
 import scala.concurrent.Future
 
@@ -14,7 +14,7 @@ import scala.concurrent.Future
 class ApprovalCommitteeRepoTest extends FlatSpec with Matchers { // AsyncFlatSpec with
 
   private val injector = GuicePlayTestApp(Seq("play.modules.reactivemongo.ReactiveMongoModule")).injector
-  private val statsService = injector.instanceOf[RequestSettingRepo]
+  private val statsService = injector.instanceOf[BatchOrderRequestSettingRepo]
 
   "add new committee" should "success" in {
 //    "a" should startWith ("a")
