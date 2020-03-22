d352d84fff068270f5977a7d26350d6b860162be
Peter Banda <peter.banda@protonmail.com>
2020-03-22 20:40:20 +0100

    Removing dummy AdaBaseController in server.

--- server/src/main/scala/org/ada/web/controllers/core/AdaBaseController.scala
+++
@@ -1,8 +0,0 @@
-//package org.ada.web.controllers.core
-//
-//import org.ada.web.models.security.DeadboltUser
-//import org.incal.play.controllers.BaseController
-//
-//abstract class AdaBaseController extends BaseController {
-//  override type USER = DeadboltUser
-//}
