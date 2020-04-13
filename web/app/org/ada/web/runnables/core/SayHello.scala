18bbdd36b65e69ddda51809a6dfd9fb25185f656
Peter Banda <peter.banda@protonmail.com>
2020-04-14 00:55:08 +0200

    SayHello runnable added (for testing purposes).

--- web/app/org/ada/web/runnables/core/SayHello.scala
+++ web/app/org/ada/web/runnables/core/SayHello.scala
@@ -1,5 +1,20 @@
 package org.ada.web.runnables.core
 
-class SayHello {
+import org.incal.core.runnables.FutureRunnable
+import play.api.Logger
+import scala.concurrent.ExecutionContext.Implicits.global
 
+import scala.concurrent.Future
+
+// TODO: Remove... just for testing
+class SayHello extends Runnable {
+  private val logger = Logger
+
+  override def run = logger.info(s"Hello at ${new java.util.Date().toString}")
+}
+
+class SayHelloFuture extends FutureRunnable {
+  private val logger = Logger
+
+  override def runAsFuture = Future(logger.info(s"Hello at ${new java.util.Date().toString}"))
 }
