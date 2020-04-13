3f15fe6a89fc8832c6846e87edd6c0d31f3b5549
Peter Banda <peter.banda@protonmail.com>
2020-04-14 00:46:20 +0200

    Runnable spec model. Relates to ada-discovery/ada-issues#62

--- server/src/main/scala/org/ada/server/models/RunnableSpec.scala
+++ server/src/main/scala/org/ada/server/models/RunnableSpec.scala
@@ -1,5 +1,34 @@
 package org.ada.server.models
 
-class RunnableSpec {
+import java.util.Date
 
+import org.ada.server.dataaccess.BSONObjectIdentity
+import org.ada.server.json.EnumFormat
+import play.api.libs.json.{Format, Json}
+import reactivemongo.bson.BSONObjectID
+import reactivemongo.play.json.BSONFormats.BSONObjectIDFormat
+
+case class RunnableSpec(
+  _id: Option[BSONObjectID],
+
+  input: Map[String, String],
+  runnableClassName: String,
+  name: String,
+
+  scheduled: Boolean = false,
+  scheduledTime: Option[ScheduledTime] = None,
+  timeCreated: Date = new Date(),
+  timeLastExecuted: Option[Date] = None
+) extends Schedulable
+
+object RunnableSpec {
+  implicit val weekDayFormat = EnumFormat(WeekDay)
+  implicit val scheduleTimeFormat = Json.format[ScheduledTime]
+
+  implicit val format = Json.format[RunnableSpec]
+
+  implicit object RunnableSpecIdentity extends BSONObjectIdentity[RunnableSpec] {
+    def of(entity: RunnableSpec): Option[BSONObjectID] = entity._id
+    protected def set(entity: RunnableSpec, id: Option[BSONObjectID]) = entity.copy(_id = id)
+  }
 }
