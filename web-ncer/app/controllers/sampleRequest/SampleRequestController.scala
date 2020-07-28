2413615d0ea712d4f5808a14d9a77b74fdc04aa8
Sascha Herzinger <sascha.herzinger@uni.lu>
2020-07-28 10:06:44 +0200

    added documentation

--- web-ncer/app/controllers/sampleRequest/SampleRequestController.scala
+++ web-ncer/app/controllers/sampleRequest/SampleRequestController.scala
@@ -3,6 +3,7 @@ package controllers.sampleRequest
 import akka.stream.Materializer
 import be.objectify.deadbolt.scala.AuthenticatedRequest
 import javax.inject.Inject
+import javax.ws.rs.BadRequestException
 import org.ada.server.models.Filter.filterConditionFormat
 import org.ada.server.models.User
 import org.ada.web.controllers.BSONObjectIDStringFormatter
@@ -25,6 +26,9 @@ case class SampleRequest(
   itemIds: Seq[BSONObjectID]
 )
 
+/**
+ * Controller that facilitates communication with REMS
+ */
 class SampleRequestController @Inject()(
   sampleRequestService: SampleRequestService
 )(
@@ -36,7 +40,16 @@ class SampleRequestController @Inject()(
   private implicit val filterConditionFormatter = JsonFormatter[FilterCondition]
   private def sampleRequestPermission(dataSet: String) = s"DS:$dataSet.dataSet"
 
-
+  /**
+   * Information needed to build a data frame for submission to REMS
+   *
+   * @param dataSetId The ID of the data set
+   * @param tableColumnNames The field names that will be submitted
+   * @param catalogueItemId The ID of a REMS catalogue item that will be target of the submission
+   * @param catalogueFormId The ID of the form item to which the data frame will be submitted to. Must be an attachment!
+   * @param selectedIds The data set row IDs selected for submission. If empty, all are selected.
+   * @param conditions The present filter conditions. If none are present, no filters are applied.
+   */
   case class SampleRequest(
     dataSetId: String,
     tableColumnNames: Seq[String],
@@ -57,6 +70,11 @@ class SampleRequestController @Inject()(
     )(SampleRequest.apply)(SampleRequest.unapply)
   )
 
+  /**
+   * Triggers request submission to REMS
+   * @param dataSetId Selected data set
+   * @return REMS URL of created sample request application
+   */
   def submitRequest(dataSetId: String) =
     restrictAdminOrPermissionAny(sampleRequestPermission(dataSetId)) { implicit  request =>
       requestForm.bindFromRequest.fold(
@@ -85,6 +103,11 @@ class SampleRequestController @Inject()(
       )
     }
 
+  /**
+   * Renders the form for user interaction
+   * @param dataSet Selected data set
+   * @return The form
+   */
   def submissionForm(dataSet: String) =
     restrictAdminOrPermissionAny(sampleRequestPermission(dataSet)) { implicit request =>
       implicit val dataSetWebCtx = dataSetWebContext(dataSet)
@@ -104,7 +127,9 @@ class SampleRequestController @Inject()(
   private def getUserForRequest()(implicit request: AuthenticatedRequest[_]) =
     for {
       deadBoltUserOption <- currentUser()
-      deadBoltUser = deadBoltUserOption.getOrElse(throw new BadRequestException("Request has no user associated with it."))
+      deadBoltUser = deadBoltUserOption.getOrElse(
+        throw new BadRequestException("Request has no user associated with it.")
+      )
     } yield deadBoltUser.user
 
 }
