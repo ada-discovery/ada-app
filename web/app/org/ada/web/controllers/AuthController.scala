22756a97e096091b0f096b313c5c1863c1dda622
Peter Banda <peter.banda@protonmail.com>
2020-08-13 23:58:45 +0200

    Open ID actions removed

--- web/app/org/ada/web/controllers/AuthController.scala
+++ web/app/org/ada/web/controllers/AuthController.scala
@@ -1,7 +1,5 @@
 package org.ada.web.controllers
 
-import java.net.UnknownHostException
-
 import javax.inject.Inject
 import play.api.{Configuration, Logger}
 import play.api.Play.current
@@ -19,11 +17,9 @@ import org.ada.web.controllers.core.AdaBaseController
 import org.incal.play.security.AuthAction
 import org.ada.web.security.AdaAuthConfig
 import play.api.libs.mailer.MailerClient
-import play.api.libs.openid.{OpenID, OpenIdClient}
 
 class AuthController @Inject() (
     val userManager: UserManager,
-    openIdClient: OpenIdClient,
     mailerClient: MailerClient
   ) extends AdaBaseController with LoginLogout with AdaAuthConfig {
 
@@ -107,70 +103,6 @@ class AuthController @Inject() (
           Unauthorized("User not found or locked.\n"),
           (userId: String) => gotoLoginSucceeded(userId, Future(Ok(s"User '${userId}' successfully logged in. Check the header for a 'PLAY_SESSION' cookie.\n")))
         )
-      }
-  }
-
-  //////////////////
-  // Open-ID Auth //
-  //////////////////
-
-  def loginOpenId = AuthAction { implicit request =>
-    Future(
-      Ok(views.html.auth.loginOpenId(openIdForm))
-    )
-  }
-
-  def loginOpenIdPostOld = Action.async { implicit request =>
-    openIdForm.bindFromRequest.fold({ error =>
-      Logger.info(s"bad request ${error.toString}")
-      Future.successful(BadRequest(error.toString))
-    }, { openId =>
-      println("Open ID: " + openId)
-
-//      openIdClient.redirectURL(
-//        openId,
-//        routes.AuthController.openIdCallback.absoluteURL()
-//      )
-
-      OpenID.redirectURL(
-        openId,
-        routes.AuthController.openIdCallback.absoluteURL()(request)
-      ).map { url =>
-        println("Redirecting to " + url)
-        Redirect(url)
-      }
-        .recover {
-          case e: UnknownHostException =>
-            Logger.error("Open ID auth failed.", e)
-            Redirect(routes.AuthController.loginOpenId).flashing("errors" -> s"Open ID auth failed. Host cannot be found.")
-
-          case t: Throwable =>
-            Logger.error("Open ID auth failed.", t)
-            Redirect(routes.AuthController.loginOpenId).flashing("errors" -> s"Open ID auth failed.")
-        }
-    })
-  }
-
-  def loginOpenIdPost =
-    Action.async { implicit request =>
-      openIdForm.bindFromRequest.fold({ error =>
-        Logger.info(s"bad request ${error.toString}")
-        Future(BadRequest(error.toString))
-      }, { openId =>
-        Future(Redirect(openId))
-      })
-    }
-
-  def openIdCallback = Action.async { implicit request =>
-    println(request.queryString.mkString("\n"))
-    openIdClient.verifiedId(request).flatMap { info =>
-      Logger.info(s"OpenId verified ${info.id}.")
-      println("Open id attributes: " + info.attributes)
-      gotoLoginSucceeded(info.id)
-    }.recover {
-        case t: Throwable =>
-          Logger.error("Open ID auth (callback) failed.", t)
-          Redirect(routes.AuthController.loginOpenId).flashing("errors" -> s"Open ID auth failed.")
       }
   }
 
