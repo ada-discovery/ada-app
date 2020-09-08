be714f1ac967df8870dbf234b71399a4cd0cdcb6
Peter Banda <peter.banda@protonmail.com>
2020-09-09 00:57:38 +0200

    ldapDn renamed to userId

--- web/test/scala/org/ada/server/models/UserSpec.scala
+++ web/test/scala/org/ada/server/models/UserSpec.scala
@@ -16,15 +16,15 @@ class UserSpec extends AsyncFlatSpec {
     val id = BSONObjectID.parse("5dc029c50c00000f059e1ccd") getOrElse fail
     val user = User(Some(id), "userName", "userName@me.com", List("ROLE_A", "ROLE_B"), List("PERM_A", "PERM_B"), true)
     val serial = Json.toJson(user).toString()
-    assert(serial == "{\"_id\":{\"$oid\":\"5dc029c50c00000f059e1ccd\"},\"ldapDn\":\"userName\",\"email\":\"userName@me.com\",\"roles\":[\"ROLE_A\",\"ROLE_B\"],\"permissions\":[\"PERM_A\",\"PERM_B\"],\"locked\":true}")
+    assert(serial == "{\"_id\":{\"$oid\":\"5dc029c50c00000f059e1ccd\"},\"userId\":\"userName\",\"email\":\"userName@me.com\",\"roles\":[\"ROLE_A\",\"ROLE_B\"],\"permissions\":[\"PERM_A\",\"PERM_B\"],\"locked\":true}")
   }
 
   it should "be de-serializable" in {
-    val serial = "{\"_id\":{\"$oid\":\"5dc029c50c00000f059e1ccd\"},\"ldapDn\":\"userName\",\"email\":\"userName@me.com\",\"roles\":[\"ROLE_A\",\"ROLE_B\"],\"permissions\":[\"PERM_A\",\"PERM_B\"],\"locked\":true}"
+    val serial = "{\"_id\":{\"$oid\":\"5dc029c50c00000f059e1ccd\"},\"userId\":\"userName\",\"email\":\"userName@me.com\",\"roles\":[\"ROLE_A\",\"ROLE_B\"],\"permissions\":[\"PERM_A\",\"PERM_B\"],\"locked\":true}"
     val json = Json.parse(serial)
     val user = Json.fromJson[User](json) getOrElse fail
     assert(user._id.getOrElse(fail).stringify == "5dc029c50c00000f059e1ccd")
-    assert(user.ldapDn == "userName")
+    assert(user.userId == "userName")
     assert(user.email == "userName@me.com")
     assert(user.roles == List("ROLE_A", "ROLE_B"))
     assert(user.permissions == List("PERM_A", "PERM_B"))
