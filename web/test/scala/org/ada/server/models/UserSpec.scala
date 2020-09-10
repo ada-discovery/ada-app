735039c18dcfd3eed91b432e0a5c4f7323d9e2a1
Peter Banda <peter.banda@protonmail.com>
2020-09-10 11:29:26 +0200

    Introducing oidcId and name fields for users. Relates to ada-discovery/ada-issues#171

--- web/test/scala/org/ada/server/models/UserSpec.scala
+++ web/test/scala/org/ada/server/models/UserSpec.scala
@@ -14,13 +14,13 @@ class UserSpec extends AsyncFlatSpec {
 
   it should "be serializable" in {
     val id = BSONObjectID.parse("5dc029c50c00000f059e1ccd") getOrElse fail
-    val user = User(Some(id), "userName", "userName@me.com", List("ROLE_A", "ROLE_B"), List("PERM_A", "PERM_B"), true)
+    val user = User(Some(id), "userName", None, "User Name", "userName@me.com", List("ROLE_A", "ROLE_B"), List("PERM_A", "PERM_B"), true)
     val serial = Json.toJson(user).toString()
-    assert(serial == "{\"_id\":{\"$oid\":\"5dc029c50c00000f059e1ccd\"},\"userId\":\"userName\",\"email\":\"userName@me.com\",\"roles\":[\"ROLE_A\",\"ROLE_B\"],\"permissions\":[\"PERM_A\",\"PERM_B\"],\"locked\":true}")
+    assert(serial == "{\"_id\":{\"$oid\":\"5dc029c50c00000f059e1ccd\"},\"userId\":\"userName\",\"name\":\"User Name\",\"email\":\"userName@me.com\",\"roles\":[\"ROLE_A\",\"ROLE_B\"],\"permissions\":[\"PERM_A\",\"PERM_B\"],\"locked\":true}")
   }
 
   it should "be de-serializable" in {
-    val serial = "{\"_id\":{\"$oid\":\"5dc029c50c00000f059e1ccd\"},\"userId\":\"userName\",\"email\":\"userName@me.com\",\"roles\":[\"ROLE_A\",\"ROLE_B\"],\"permissions\":[\"PERM_A\",\"PERM_B\"],\"locked\":true}"
+    val serial = "{\"_id\":{\"$oid\":\"5dc029c50c00000f059e1ccd\"},\"userId\":\"userName\",\"name\":\"User Name\",\"email\":\"userName@me.com\",\"roles\":[\"ROLE_A\",\"ROLE_B\"],\"permissions\":[\"PERM_A\",\"PERM_B\"],\"locked\":true}"
     val json = Json.parse(serial)
     val user = Json.fromJson[User](json) getOrElse fail
     assert(user._id.getOrElse(fail).stringify == "5dc029c50c00000f059e1ccd")
