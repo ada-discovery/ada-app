136d2100c7013005e484e54198a6feeb3e42bd77
Peter Banda <peter.banda@protonmail.com>
2020-09-21 21:05:37 +0200

    Adjusting DsaInputFutureRunnable using a new async/future creation. Relates to ada-discovery/ada-issues#171

--- server/src/main/scala/org/ada/server/runnables/DsaInputFutureRunnable.scala
+++ server/src/main/scala/org/ada/server/runnables/DsaInputFutureRunnable.scala
@@ -5,14 +5,13 @@ import org.ada.server.AdaException
 import org.incal.core.runnables.InputFutureRunnableExt
 import org.ada.server.dataaccess.dataset.DataSetAccessorFactory
 import scala.reflect.runtime.universe.TypeTag
+import scala.concurrent.ExecutionContext.Implicits.global
 
 abstract class DsaInputFutureRunnable[I](implicit override val typeTag: TypeTag[I]) extends InputFutureRunnableExt[I] {
 
   @Inject var dsaf: DataSetAccessorFactory = _
 
-  protected def createDsa(dataSetId: String) = dsaf.applySync(dataSetId).getOrElse(
-    throw new AdaException(s"Data set id ${dataSetId} not found.")
-  )
+  protected def createDsa(dataSetId: String) = dsaf.getOrError(dataSetId)
 
-  protected def createDataSetRepo(dataSetId: String) = createDsa(dataSetId).dataSetRepo
+  protected def createDataSetRepo(dataSetId: String) = createDsa(dataSetId).map(_.dataSetRepo)
 }
