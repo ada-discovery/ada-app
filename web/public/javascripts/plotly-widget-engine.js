0de63d64aab6f079eda312cb0d3aebc16703d363
Peter Banda <peter.banda@protonmail.com>
2021-01-28 09:16:26 +0100

    PlotlyWidgetEngine - AreaSelected and AreaZoomed events handling for the scatter widget

--- web/public/javascripts/plotly-widget-engine.js
+++ web/public/javascripts/plotly-widget-engine.js
@@ -35,6 +35,10 @@ class HighchartsWidgetEngine extends HighchartsWidgetEnginex {
             xDataType,
             yDataType
         })
+
+        if (filterElement) {
+            this._addScatterAreaZoomed(elementId, filterElement, widget, isXDate, isYDate);
+        }
     }
 
     _scatterChart({
@@ -73,5 +77,65 @@ class HighchartsWidgetEngine extends HighchartsWidgetEnginex {
         };
 
         Plotly.newPlot(chartElementId, series, layout);
+
+        this._addScatterAreaSelected(chartElementId)
+    }
+
+    _addScatterAreaZoomed(elementId, filterElement, widget, isXDate, isYDate) {
+        $("#" + elementId).get(0).on('plotly_relayout', function(eventData) {
+            const xMin = eventData["xaxis.range[0]"]
+            const xMax = eventData["xaxis.range[1]"]
+            const yMin = eventData["yaxis.range[0]"]
+            const yMax = eventData["yaxis.range[1]"]
+
+            if (xMin) {
+                const xMinOut = (isXDate) ? msOrDateToStandardDateString(xMin) : xMin.toString();
+                const xMaxOut = (isXDate) ? msOrDateToStandardDateString(xMax) : xMax.toString();
+                const yMinOut = (isYDate) ? msOrDateToStandardDateString(yMin) : yMin.toString();
+                const yMaxOut = (isYDate) ? msOrDateToStandardDateString(yMax) : yMax.toString();
+
+                var conditions = [
+                    {fieldName: widget.xFieldName, conditionType: ">=", value: xMinOut},
+                    {fieldName: widget.xFieldName, conditionType: "<=", value: xMaxOut},
+                    {fieldName: widget.yFieldName, conditionType: ">=", value: yMinOut},
+                    {fieldName: widget.yFieldName, conditionType: "<=", value: yMaxOut}
+                ]
+
+                $(filterElement).multiFilter('addConditionsAndSubmit', conditions);
+            }
+        });
+    }
+
+    _addScatterAreaSelected(elementId) {
+        const element = $("#" + elementId).get(0)
+
+        element.on('plotly_selected', function(eventData) {
+            if (element.layout.annotations) {
+                Plotly.relayout(elementId, 'annotations[0]', 'remove');
+            }
+
+            if (eventData) {
+                const count = eventData.points.length
+
+                const xMin = element.layout.xaxis.range[0]
+                const yMin = element.layout.yaxis.range[0]
+                const xMax = element.layout.xaxis.range[1]
+                const yMax = element.layout.yaxis.range[1]
+
+                const newAnnotation = {
+                    x: xMax - 0.1 * (xMax - xMin),
+                    y: yMax - 0.1 * (yMax - yMin),
+                    showarrow: false,
+                    bgcolor: 'rgba(255, 255, 255, 0.9)',
+                    font: {size: 12},
+                    borderwidth: 2,
+                    borderpad: 4,
+                    bordercolor: 'dimGrey',
+                    text: '<i># points selected:</i><b>' + count + '</b>'
+                }
+
+                Plotly.relayout(elementId, 'annotations[0]', newAnnotation);
+            }
+        });
     }
 }
