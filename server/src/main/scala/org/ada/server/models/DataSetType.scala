b292df6a55a12778ef080bfaea10af719b2b80e0
luca.bolzani <luca.bolzani@uni.lu>
2022-04-13 16:35:30 +0200

    Update DataSetType from "Omics" to "Joinable" name

--- server/src/main/scala/org/ada/server/models/DataSetType.scala
+++ server/src/main/scala/org/ada/server/models/DataSetType.scala
@@ -3,7 +3,7 @@ package org.ada.server.models
 import play.api.libs.json.{Reads, Writes}
 
 object DataSetType extends Enumeration {
-  val omics = Value("Omics")
+  val joinable = Value("Joinable")
 
   implicit val dataSetTypeRead: Reads[DataSetType.Value] = Reads.enumNameReads(DataSetType)
   implicit val dataSetTypeWrite: Writes[DataSetType.Value] = Writes.enumNameWrites
--- web/app/org/ada/web/controllers/dataset/omics/OmicsDataSetController.scala
+++ web/app/org/ada/web/controllers/dataset/omics/OmicsDataSetController.scala
@@ -43,7 +43,7 @@ class OmicsDataSetController @Inject()(
       dSettings <- dataSettings
       mInfos <- dataSetMetaInfos
       if dSettings.dataSetId == mInfos.id &&
-         dSettings.dataSetInfo.get.dataSetType == DataSetType.omics
+         dSettings.dataSetInfo.get.dataSetType == DataSetType.joinable
     } yield mInfos
 
     for {mInfoFilter <- mInfosFiltered} yield {
